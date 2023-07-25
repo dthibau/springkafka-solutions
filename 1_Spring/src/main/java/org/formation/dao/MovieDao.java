d12842756b08cff8bb6b91b8b198a12bd3ab9f89
David THIBAU <david.thibau@gmail.com>
2023-07-25 12:14:33 +0200

    Départ Atelier1

---
+++ 1_Spring/src/main/java/org/formation/dao/MovieDao.java
@@ -0,0 +1,7 @@
+package org.formation.dao;
+
+import org.formation.model.Movie;
+
+public interface MovieDao {
+    public List<Movie> findAll();
+}
---
+++ 1_Spring/src/main/java/org/formation/model/Movie.java
@@ -0,0 +1,9 @@
+package org.formation.model;
+
+import lombok.Data;
+
+@Data
+public class Movie {
+    private String title,director;
+    private int duration;
+}
