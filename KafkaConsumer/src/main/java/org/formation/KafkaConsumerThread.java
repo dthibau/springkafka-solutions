be16212404ccdb3dd13d260e2aceb3b0f52bfef4
David THIBAU <david.thibau@gmail.com>
2023-07-31 16:36:34 +0200

    Départ Atelier Consumer

---
+++ KafkaConsumer/src/main/java/org/formation/KafkaConsumerThread.java
@@ -0,0 +1,42 @@
+package org.formation;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.formation.model.Coursier;
+
+public class KafkaConsumerThread implements Runnable {
+
+	public static String TOPIC = "position";
+	KafkaConsumer<String, Coursier> consumer;
+	private int id;
+
+	
+
+	public KafkaConsumerThread(int id) {
+		this.id = id;
+
+		_initConsumer();
+
+	}
+
+	@Override
+	public void run() {
+		int nbMessages = 0;
+		try {
+			while (true) {
+
+				// A compléter
+				
+			}
+		} finally {
+			consumer.close();
+		}
+
+	}
+
+	private void _initConsumer() {
+
+	}
+}
