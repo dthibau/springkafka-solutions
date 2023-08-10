86d1a584c933014486e587102a94dd65f04ddbb7
David THIBAU <david.thibau@gmail.com>
2023-08-10 11:11:29 +0200

    Revert "Interaction RestFul"
    This reverts commit 4384c5c7a9595178f0f559d7d77716c03df4a9ea.

--- OrderService/src/main/java/org/formation/service/OrderService.java
+++ OrderService/src/main/java/org/formation/service/OrderService.java
@@ -5,31 +5,22 @@ import java.time.Instant;
 import org.formation.domain.Order;
 import org.formation.domain.OrderRepository;
 import org.formation.domain.OrderStatus;
-import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import jakarta.transaction.Transactional;
 
 @Service
 @Transactional
 public class OrderService {
-	private final OrderRepository orderRepository;
-	private final RestTemplate restTemplate;
 
-	public OrderService(OrderRepository orderRepository, RestTemplateBuilder restTemplateBuilder) {
-		this.orderRepository = orderRepository;
-		this.restTemplate = restTemplateBuilder.rootUri("http://localhost:8082").build();
-	}
+	@Autowired
+	OrderRepository orderRepository;
+	
 	
 	public Order createOrder(Order order) {	
 		order.setDate(Instant.now());
 		order.setStatus(OrderStatus.PENDING);
-
-		order = orderRepository.save(order);
-		
-		restTemplate.postForEntity("/api/tickets", new TicketDto(order), Object.class);
-
-		return order;
+		return orderRepository.save(order);
 	}
 }
