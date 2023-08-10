68e9ed478466d0bb90c656e29ffc197718c88549
David THIBAU <david.thibau@gmail.com>
2023-08-10 10:25:43 +0200

    Départ Atelier KafkaTemplate

--- OrderService/src/main/java/org/formation/event/OrderEventRepository.java
+++
@@ -1,6 +0,0 @@
-package org.formation.event;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderEventRepository extends JpaRepository<OrderEvent, Long> {
-}
--- OrderService/src/main/java/org/formation/event/OrderEventType.java
+++
@@ -1,8 +0,0 @@
-package org.formation.event;
-
-public enum OrderEventType {
-    ORDER_CREATED,
-    ORDER_PAID,
-    ORDER_CANCELLED ,
-    ORDER_DELIVERED
-}
