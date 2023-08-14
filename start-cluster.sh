84118f63224a0f362e7aeabd6e487dbd1d7601fc
David THIBAU <david.thibau@gmail.com>
2023-08-14 10:15:00 +0200

    SSL pour communications inter-broker

--- start-cluster.sh
+++ start-cluster.sh
@@ -4,6 +4,7 @@ export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64/
 export KAFKA_DIST=/home/dthibau/Formations/SpringKafka/MyWork/kafka-dist/kafka_2.13-3.2.1
 export KAFKA_CLUSTER=/home/dthibau/Formations/SpringKafka/github/solutions/kafka-cluster
 export KAFKA_LOGS=/home/dthibau/Formations/SpringKafka/MyWork/kafka-logs
+export KAFKA_OPTS="-Djavax.net.debug=ssl:handshake:verbose"
 
 
 $KAFKA_DIST/bin/kafka-server-start.sh -daemon $KAFKA_CLUSTER/broker-1/server.properties 
