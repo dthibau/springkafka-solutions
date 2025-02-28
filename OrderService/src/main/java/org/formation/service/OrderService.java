c7caa5e9fd018636173882c7fdad488735e28c45
David THIBAU <david.thibau@gmail.com>
2025-02-28 11:31:23 +0100

    Patterns

--- OrderService/src/main/java/org/formation/service/OrderService.java
+++ OrderService/src/main/java/org/formation/service/OrderService.java
@@ -6,6 +6,9 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.SneakyThrows;
 import org.formation.domain.*;
+import org.formation.event.OrderEvent;
+import org.formation.event.OrderEventRepository;
+import org.formation.event.OrderEventType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,17 +18,27 @@ import jakarta.transaction.Transactional;
 @Transactional
 public class OrderService {
 
-	@Autowired
-	OrderRepository orderRepository;
+	private final OrderRepository orderRepository;
+	private final OrderEventRepository eventRepository;
+    private final ObjectMapper mapper;
 
+	public OrderService(OrderRepository orderRepository, OrderEventRepository eventRepository, ObjectMapper mapper) {
+		this.orderRepository = orderRepository;
+		this.eventRepository = eventRepository;
+		this.mapper = mapper;
+	}
 
-	@Autowired
-	ObjectMapper mapper;
-	
-	public Order createOrder(Order order)  {
+	@SneakyThrows
+    public Order createOrder(Order order)  {
 		order.setDate(Instant.now());
 		order.setStatus(OrderStatus.PENDING);
 		order = orderRepository.save(order);
+
+		OrderEvent event = OrderEvent.builder().type(OrderEventType.ORDER_CREATED)
+				.orderId(order.getId())
+				.payload(mapper.writeValueAsString(order))
+				.build();
+		eventRepository.save(event);
 
 		return order;
 	}
