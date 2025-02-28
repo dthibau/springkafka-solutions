fbb94d663668f9f89024ada907b17c1b5ab45db5
David THIBAU <david.thibau@gmail.com>
2025-02-28 10:54:07 +0100

    Autoconfiguration

--- OrderService/src/main/java/org/formation/Application.java
+++ OrderService/src/main/java/org/formation/Application.java
@@ -2,14 +2,28 @@ package org.formation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 @EnableScheduling
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+
+		ApplicationContext context = SpringApplication.run(Application.class, args);
+		Arrays.stream(context.getBeanDefinitionNames())
+				.filter(beanName -> beanName.toLowerCase().contains("kafka"))
+				.forEach(System.out::println);
+
+		DefaultKafkaProducerFactory kafkaProducerFactory = (DefaultKafkaProducerFactory)context.getBean("kafkaProducerFactory");
+		kafkaProducerFactory.getConfigurationProperties().forEach((k,v) -> System.out.println(k + " : " + v));
+
+
 	}
 
 }
