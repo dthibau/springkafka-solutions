ec3382752ff80dfc22a80d3fb203f61926ed6ee3
David THIBAU <david.thibau@gmail.com>
2025-03-03 13:51:01 +0100

    DeadLetter Topic

--- DeliveryService/src/main/java/org/formation/KafkaConfig.java
+++ DeliveryService/src/main/java/org/formation/KafkaConfig.java
@@ -9,16 +9,18 @@ import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.config.TopicConfig;
 import org.formation.domain.Position;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.TopicBuilder;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.kafka.core.RoutingKafkaTemplate;
+import org.springframework.kafka.core.*;
+import org.springframework.kafka.listener.CommonErrorHandler;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.util.backoff.FixedBackOff;
 
 @Configuration
 public class KafkaConfig {
@@ -44,6 +46,12 @@ public class KafkaConfig {
         map.put(Pattern.compile(coursierChannel), coursierProducerFactory);
         map.put(Pattern.compile(".+"), defaultProducerFactory);
         return new RoutingKafkaTemplate(map);
+    }
+
+    @Bean
+    public CommonErrorHandler errorHandler(@Qualifier("kafkaProducerFactory") ProducerFactory<Long, Object> pf) {
+        return new DefaultErrorHandler(
+                new DeadLetterPublishingRecoverer(new KafkaTemplate<>(pf)), new FixedBackOff(1000L, 4));
     }
 
     @Bean
